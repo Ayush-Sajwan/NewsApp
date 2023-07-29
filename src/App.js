b4850371ab2d828870b8a69fb1de134fcfa93b6b
Ayush <ayushsajwan19@gmail.com>
2023-07-29 13:47:46 +0530

    Deleted the logo files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  name='Ayush';
+  render() {
+    return (
+      <div>
+        <h1>This is our class based react app</h1>
+        <h2>Created by {this.name}</h2>
+      </div>
+    )
+  }
 }
 
-export default App;
